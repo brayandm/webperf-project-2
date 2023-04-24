16ef812f45b5d99b7bdd5efb4b9ec07639905217
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-04-24 19:55:48 +0200

    Fix parallax in the Hero to not repaint constantly.

--- src/scenes/HomePage/components/Hero.tsx
+++ src/scenes/HomePage/components/Hero.tsx
@@ -3,32 +3,35 @@ import { Link } from "react-router-dom";
 import s from "./Hero.module.css";
 
 export const Hero = () => {
-  const [parallaxY, setParallaxY] = useState(0);
+    const [parallaxY, setParallaxY] = useState(0);
 
-  const onScroll = useCallback(() => {
-    setParallaxY(window.scrollY / 10);
-  }, [setParallaxY]);
+    const onScroll = useCallback(() => {
+        setParallaxY(window.scrollY / 10);
+    }, [setParallaxY]);
 
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, [onScroll]);
+    useEffect(() => {
+        window.addEventListener("scroll", onScroll);
+        return () => {
+            window.removeEventListener("scroll", onScroll);
+        };
+    }, [onScroll]);
 
-  return (
-    <section className={s.container}>
-      <div className={s.imageContainer} style={{ top: -parallaxY }}>
-        <img className={s.image} src="bg.jpg" alt="" />
-      </div>
-      <div className={s.center}>
-        <h1 className={s.title}>Star fighter</h1>
-        <Link className={s.button} to="/game">
-          Start
-        </Link>
-      </div>
-    </section>
-  );
+    return (
+        <section className={s.container}>
+            <div
+                className={s.imageContainer}
+                style={{ transform: `translateY(-${parallaxY}px)` }}
+            >
+                <img className={s.image} src="bg.jpg" alt="" />
+            </div>
+            <div className={s.center}>
+                <h1 className={s.title}>Star fighter</h1>
+                <Link className={s.button} to="/game">
+                    Start
+                </Link>
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
