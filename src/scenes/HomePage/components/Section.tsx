128164e1ce306d783ae9bd71f187111f3c663138
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-04-24 13:17:07 +0200

    lodash-es added

--- src/scenes/HomePage/components/Section.tsx
+++ src/scenes/HomePage/components/Section.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import _ from "lodash";
+import { capitalize } from "lodash-es";
 import s from "./Section.module.css";
 
 interface SectionProps {
@@ -10,7 +10,7 @@ interface SectionProps {
 export const Section = ({ title, body }: SectionProps) => {
   return (
     <section className={s.container}>
-      <h2 className={s.title}>{_.capitalize(title)}</h2>
+      <h2 className={s.title}>{capitalize(title)}</h2>
       <p className={s.paragraph}>{body}</p>
     </section>
   );
