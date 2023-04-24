c1f4a58f8d72a3715678d067dbaa281c436c3125
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-04-24 15:58:18 +0200

    Don't lazy load the Hero section (Fix JS waterfall).

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./styles.css";
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Hero } from "./scenes/HomePage/components/Hero";
 
 const HomePage = lazy(() => import("./scenes/HomePage"));
 const VideoGame = lazy(() => import("./scenes/VideoGame"));
 
 export default function App() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Hero />}>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/game" element={<VideoGame />} />
--- src/scenes/HomePage/components/index.ts
+++ src/scenes/HomePage/components/index.ts
@@ -1,7 +1,4 @@
-import { lazy } from "react";
-
-export const Hero = lazy(() => import("./Hero"));
-
+export * from "./Hero";
 export * from "./Section";
 export * from "./Form";
 export * from "./Container";
